06972286dd37c47e5977f6ea2f973521b4686b7a
Wojciech Dudek <wdudek82@gmail.com>
2017-07-04 23:42:17 +0200

    Arrow functions

--- src/js/arrowFunction.js
+++ src/js/arrowFunction.js
@@ -1,3 +1,24 @@
-/**
- * Created by neevor on 04/07/17.
- */
+let arr = [1,2,3,4,5];
+
+let mul = e => console.log(e**e);
+
+function mulLong(num) {
+    console.log(num**num);
+}
+
+arr.map(mul);
+arr.map(mulLong);
+
+
+// Arrow Function Variations
+let noArgs = () => 3;
+let placeholderArg = _ => 3;
+let args = (x, y) => 3;
+let statement = x => { try { 3; } catch(e) {} };
+let multiLine = x => {
+  return x;  // no implicit return
+};
+let withObject = x => ({x: 3});
+
+
+// Exercise
