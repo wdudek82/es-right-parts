cb62687b47d965ed7985e6409801686aedc74b38
Wojciech Dudek <wdudek82@gmail.com>
2017-07-09 20:00:30 +0200

    Concise objects/classes, generators inside of a class

---
+++ src/js/concise_props_methods.js
@@ -0,0 +1,35 @@
+class TestObj {
+    constructor() {
+        this.state = {
+            active: 1
+        };
+    }
+
+    foo(n) {
+        if (n > 1) {
+            return this.foo(n-1) * n;
+        } else {
+            return 1
+        }
+    };
+
+    *bar() {
+        let n = 1;
+        while (true) {
+            yield n++;
+        }
+    };
+}
+
+
+const instance = new TestObj();
+
+let baz = instance.bar();
+
+console.log(instance.foo(4));
+console.log(instance.state);
+console.log(baz.next().value);
+console.log(baz.next().value);
+console.log(baz.next().value);
+console.log(baz.next().value);
+console.log(baz.next().value);
