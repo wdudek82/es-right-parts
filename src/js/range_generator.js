d2d59c91c2cff189a1438646d0d07349e9223bca
Wojciech Dudek <wdudek82@gmail.com>
2017-07-12 23:45:04 +0200

    Python's range implementation in ES6 with generator

--- src/js/range_generator.js
+++ src/js/range_generator.js
@@ -1,12 +1,17 @@
-let range = {
-    *[Symbol.iterator](start = 0, stop = 101, step = 1) {
-        for (let i = start; i < stop; i += step) {
-            yield i;
-        }
+// let range = {
+//     *[Symbol.iterator](start = 0, stop = 101, step = 1) {
+//         for (let i = start; i < stop; i += step) {
+//             yield i;
+//         }
+//     }
+// };
+
+function* range(start = 0, stop = 101, step = 1) {
+    for (let i = start; i < stop; i += step) {
+        yield i;
     }
-};
+}
 
-
-let newRange = [ ...range ];
+let newRange = [ ...range(5, 50, 4) ];
 
 console.log(newRange);
